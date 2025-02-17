ac538229aa0ea68006afceae36b25013b64b75d6
semzz <semzzcs@gmail.com>
2025-02-17 11:02:33 +0300

    Yдалить комментарии в коде

--- web-programming/lab1/script.js
+++ web-programming/lab1/script.js
@@ -1,8 +1,6 @@
-// Функция для переключения отображения меню
 function toggleMenu() {
     const menu = document.getElementById("support-menu");
     
-    // Если меню скрыто, показываем его, если видно - скрываем
     if (menu.style.display === "none" || menu.style.display === "") {
         menu.style.display = "block";
     } else {
